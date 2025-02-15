ae89d058ade63c0bbc974f8a7f5e2e3c1a3ed229
Tenebris2 <buiducanh567@gmail.com>
2025-02-15 10:06:24 +0700

    Added example

--- scripts/scripts/source-code-extractor/extract.sh
+++
@@ -1,5 +0,0 @@
-objdump -dGlS $1 > trace.dump
-OUTPUT_FILE="source_code_extraction_time.txt"
-{ time python3 ./scripts/extract_addresses.py trace.dump; } 2> "$OUTPUT_FILE"
-
-cp addresses $AURORA_GIT_DIR/tracing/scripts/ -r
